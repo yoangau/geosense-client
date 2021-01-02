ec9d58b8319abe7531aa746ef43a2ec13accf0b7
Yoan Gauthier <yoangau@gmail.com>
2021-01-02 17:45:29 -0500

    add font

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,21 @@
 import React from "react"
 import ReactDOM from "react-dom"
 import { Provider } from "react-redux"
+import { Global, css } from "@emotion/react"
 import App from "./app/app"
 import { store } from "./app/store"
 import * as serviceWorker from "./serviceWorker"
+import "@fontsource/roboto-mono"
 
 ReactDOM.render(
   <React.StrictMode>
+    <Global
+      styles={css`
+        body {
+          font-family: "Roboto Mono";
+        }
+      `}
+    />
     <Provider store={store}>
       <App />
     </Provider>
