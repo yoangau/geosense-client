eb4629c91ae61339ce4bc4a6d72a3239e313e426
Yoan Gauthier <yoangau@gmail.com>
2021-01-02 17:45:50 -0500

    create minimal home screen

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -5,6 +5,7 @@ import Home from "../features/home/home"
 
 const AppContainer = styled.div`
   display: flex;
+  flex-direction: column;
   align-items: center;
   justify-content: center;
   max-width: 100vw;
--- src/features/home/home.tsx
+++ src/features/home/home.tsx
@@ -1,6 +1,24 @@
 import React from "react"
-import { Text } from "@geist-ui/react"
+import { Text, Button } from "@geist-ui/react"
+import { MapPin, Map, Shuffle } from "@geist-ui/react-icons"
+import styled from "@emotion/styled"
 
-export const Home = () => <Text h1>Geo Sense</Text>
+const SimpleButton = styled(Button)`
+  margin-bottom: 5px;
+`
+
+export const Home = () => (
+  <>
+    <Text h1>
+      <MapPin /> Geosense
+    </Text>
+    <SimpleButton type="secondary" ghost icon={<Map />}>
+      Create A Game
+    </SimpleButton>
+    <SimpleButton type="secondary" ghost icon={<Shuffle />}>
+      Random game
+    </SimpleButton>
+  </>
+)
 
 export default Home
