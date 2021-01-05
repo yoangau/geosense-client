8600bbed7017116363c8fc6101ba0d691d69f660
Yoan Gauthier <yoangau@gmail.com>
2021-01-04 20:45:10 -0500

    use useAsync in globe and game map

--- src/features/game/game-map.tsx
+++ src/features/game/game-map.tsx
@@ -1,46 +1,52 @@
-import React, { useEffect, useState } from "react"
+import React, { useState } from "react"
 import { geoPath } from "d3-geo"
 import { geoPatterson } from "d3-geo-projection"
 import { feature } from "topojson-client"
 import styled from "@emotion/styled"
+import useAsync from "../../hooks/use-async"
+
+const width = 600
+const height = 400
 
 const Map = styled.svg`
   stroke: #fff;
   stroke-width: 0.2;
 `
 
+const loadMap = () =>
+  fetch("countries-110m.json")
+    .then(response => response.json())
+    .then(jsonData => {
+      const geoJson = feature(jsonData, jsonData.objects.countries)
+      const projection = geoPatterson().fitSize([width, height], geoJson)
+
+      const geoGenerator = geoPath().projection(projection)
+
+      const pathString = geoGenerator(geoJson)
+      return { pathString, invertProjection: projection.invert }
+    })
+
 export const GameMap = () => {
-  const [geoJson, changeGeoJson] = useState<any>()
   const [points, changePoints] = useState<[number, number][]>([])
-  const width = 600
-  const height = 400
+  const { value } = useAsync(loadMap)
+
+  if (!value) return <></>
+
+  const { pathString, invertProjection } = value
+
   const r = 4
-
-  useEffect(() => {
-    fetch("countries-110m.json")
-      .then(response => response.json())
-      .then(jsonData => {
-        changeGeoJson(feature(jsonData, jsonData.objects.countries))
-      })
-  }, [])
-
-  const projection = geoPatterson().fitSize([width, height], geoJson)
-
-  const geoGenerator = geoPath().projection(projection)
-
-  const pathString = geoGenerator(geoJson)
 
   const addPoint = (event: any) => {
     const point: [number, number] = [+event.nativeEvent.layerX, +event.nativeEvent.layerY]
     changePoints([...points, point])
-    console.log(projection.invert(point))
+    console.log(invertProjection(point))
   }
 
   return (
     <Map width={width} height={height} onClick={e => addPoint(e)}>
       <path d={pathString as string} />
-      {points.map(([x, y]) => (
-        <circle cx={x} cy={y} r={r} stroke="blue" fill="green" />
+      {points.map(([x, y], i) => (
+        <circle key={`dot-${i}`} cx={x} cy={y} r={r} stroke="blue" fill="green" />
       ))}
     </Map>
   )
--- src/features/home/globe.tsx
+++ src/features/home/globe.tsx
@@ -1,25 +1,31 @@
 import React, { useEffect, useState } from "react"
-import { geoOrthographic, geoPath } from "d3-geo"
+import { ExtendedGeometryCollection, GeoGeometryObjects, geoOrthographic, geoPath } from "d3-geo"
 import { feature } from "topojson-client"
 import { presimplify, simplify } from "topojson-simplify"
+import useAsync from "../../hooks/use-async"
+
+const loadMap = () =>
+  fetch("land-110m.json")
+    .then(response => response.json())
+    .then(jsonData => {
+      const simpleTopo = simplify(presimplify(jsonData), 10)
+      return (feature(
+        simpleTopo,
+        simpleTopo.objects.land,
+        // weird type interaction between topojson.feature and d3-geo.GeoProjection.fitSize
+      ) as unknown) as ExtendedGeometryCollection<GeoGeometryObjects>
+    })
 
 export const Globe = () => {
+  const { value: geoJson } = useAsync(loadMap)
   const [rotation, changeRotation] = useState(0)
-  const [geoJson, changeGeoJson] = useState<any>()
-  const size = 300
-
-  useEffect(() => {
-    fetch("land-110m.json")
-      .then(response => response.json())
-      .then(jsonData => {
-        const simpleTopo = simplify(presimplify(jsonData), 10)
-        changeGeoJson(feature(simpleTopo, simpleTopo.objects.land))
-      })
-  }, [])
-
   useEffect(() => {
     setTimeout(() => changeRotation(rotation + 0.2), 10)
   }, [rotation])
+
+  if (!geoJson) return <></>
+
+  const size = 300
 
   const projection = geoOrthographic().fitSize([size, size], geoJson).rotate([rotation, 0])
 
