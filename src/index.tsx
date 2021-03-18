caaac242a7602a1b4d63132b4d14dc1dc9d15347
Yoan Gauthier <yoangau@gmail.com>
2021-03-18 18:18:14 -0400

    remove service worker from index

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,6 @@ import React from "react"
 import ReactDOM from "react-dom"
 import { Global, css } from "@emotion/react"
 import App from "./app/app"
-import * as serviceWorker from "./serviceWorker"
 import "@fontsource/roboto-mono"
 
 ReactDOM.render(
@@ -23,8 +22,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById("root"),
 )
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister()
