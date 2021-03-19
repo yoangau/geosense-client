56c29a143583a360672eb94a94b04e8e9b3f5a0e
Yoan Gauthier <yoangau@gmail.com>
2021-03-19 00:05:45 -0400

    adjust root component with AuthRoutes

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,8 +1,9 @@
-import React from "react"
+import React, { Suspense } from "react"
 import { HashRouter as Router, Switch, Route } from "react-router-dom"
 import styled from "@emotion/styled"
 import { Home, Group, Game } from "../features"
-import UserRegister from "../features/user/user-register"
+import { AuthRoute } from "../features/auth/auth-route"
+import DebugObserver from "../helpers/debug-observer"
 
 const AppContainer = styled.div`
   display: flex;
@@ -17,14 +18,16 @@ const AppContainer = styled.div`
 export const App = () => {
   return (
     <AppContainer>
-      <Router>
-        <Switch>
-          <Route path="/user" component={UserRegister} />
-          <Route path="/lobby" component={Group} />
-          <Route path="/game" component={Game} />
-          <Route path="/" component={Home} />
-        </Switch>
-      </Router>
+      <DebugObserver />
+      <Suspense fallback={<div>Loading...</div>}>
+        <Router>
+          <Switch>
+            <AuthRoute path="/lobby" component={Group} />
+            <AuthRoute path="/game" component={Game} />
+            <Route path="/" component={Home} />
+          </Switch>
+        </Router>
+      </Suspense>
     </AppContainer>
   )
 }
