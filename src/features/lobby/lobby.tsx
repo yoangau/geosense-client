e989488a179cd3d3e1c7b80ef8bdad1f067d8daa
Yoan Gauthier <yoangau@gmail.com>
2021-03-20 12:03:35 -0400

    add socket context to lobby

--- src/features/lobby/lobby.tsx
+++ src/features/lobby/lobby.tsx
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 import { Button, Table, Text } from "@geist-ui/react"
 import { Users, User as GeistUser, UserCheck, Play } from "@geist-ui/react-icons"
 import styled from "@emotion/styled"
@@ -8,6 +8,8 @@ import { userState } from "../user/user.atom"
 import HttpApi from "../../api/http-api"
 import { User } from "../../@types"
 import { lobbyState } from "./lobby.atom"
+import { SocketContext } from "../../components/socket-context/socket.context"
+import SocketApi, { SocketOnType } from "../../api/socket-api"
 
 const SmallTable = styled.div`
   max-width: min(90vw, 600px);
@@ -17,11 +19,21 @@ const SmallTable = styled.div`
 const httpApi = new HttpApi()
 
 export const Lobby = () => {
+  const [socket, setSocket] = useState<SocketApi | undefined>()
+
   const history = useHistory()
   const { lobbyId } = useParams<{ lobbyId: string | undefined }>()
   const user = useRecoilValue(userState) as User
   const lobby = useRecoilValue(lobbyState)
   const setLobby = useSetRecoilState(lobbyState)
+
+  useEffect(() => {
+    setSocket(
+      new SocketApi({
+        [SocketOnType.Update]: console.log,
+      }),
+    )
+  }, [])
 
   if (!lobbyId && !lobby) {
     httpApi.createLobby(user.id).then(lob => {
@@ -63,19 +75,21 @@ export const Lobby = () => {
   ]
   return (
     <>
-      <Text h1>
-        <Users /> Lobby
-      </Text>
-      <SmallTable>
-        <Table data={data}>
-          <Table.Column prop="player" label="player" />
-          <Table.Column prop="status" label="status" />
-          <Table.Column prop="control" label="control" />
-        </Table>
-      </SmallTable>
-      <Button type="secondary" auto ghost icon={<Play />} onClick={() => history.push("/game")}>
-        Start Game
-      </Button>
+      <SocketContext.Provider value={socket}>
+        <Text h1>
+          <Users /> Lobby
+        </Text>
+        <SmallTable>
+          <Table data={data}>
+            <Table.Column prop="player" label="player" />
+            <Table.Column prop="status" label="status" />
+            <Table.Column prop="control" label="control" />
+          </Table>
+        </SmallTable>
+        <Button type="secondary" auto ghost icon={<Play />} onClick={() => history.push("/game")}>
+          Start Game
+        </Button>
+      </SocketContext.Provider>
     </>
   )
 }
